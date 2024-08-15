de6e32a56647465a863f5b5c7a9b3eaf5bfe7a0b
imFAHADcom <contact@imfahad.com>
2024-08-15 13:25:03 +0300

    FIX string_homework.cpp

--- string_homework.cpp
+++ string_homework.cpp
@@ -13,12 +13,15 @@ int main() {
 	std::cout << "Please Enter string3? ";
 	std::cin >> string3;
 
+
+	// calculate
+	int result = stoi(string2) * stoi(string3);
 	//outputs
 	std::cout << "********************************\n";
 	std::cout << "The Length of string1 is: " << string1.length() << "\n";
 	std::cout << "Characters string1 at 0,2,4,7 are: " << string1[0] << " "  << string1[2] << " " << string1[4] << " " << string1[7] << " " << "\n";
 	std::cout << "Concatenating string2 and string3 = " << string2 + string3 << "\n";
-	std::cout << string2 << " * " << string3 << " = " << stoi(string2) * stoi(string3) << std::endl;
+	std::cout << string2 << " * " << string3 << " = " << result  << std::endl;
 
 	return 0;
 };
