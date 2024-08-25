1ef9eef08eeecb01eda153e826e8fd037dc9d9d6
imFAHADcom <contact@imfahad.com>
2024-08-25 20:18:44 +0300

    FIX call_by_value.cpp

--- call_by_value.cpp
+++ call_by_value.cpp
@@ -3,7 +3,7 @@
 void function(int x) {
 
 	x = 20;
-	std::cout << "Number inside function became= " << x << std::endl;
+	std::cout << "variable value inside the function became= " << x << std::endl;
 }
 
 
@@ -12,11 +12,11 @@ int main() {
 	int a;
 	a = 10;
 
-	std::cout << "Number before calling the function became= " << a << std::endl;
+	std::cout << "a variable before calling the function became= " << a << std::endl;
 
 	function(a);
 
-	std::cout << "Number after calling the function became= " << a << std::endl;
+	std::cout << "a variable after calling the function became= " << a << std::endl;
 	
 	return 0;
 }
