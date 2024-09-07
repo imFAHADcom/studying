3ec06b8d340450a2a50bbb626c6ac4904c546cc6
imFAHADcom <contact@imfahad.com>
2024-09-08 00:15:30 +0300

    ADD array_int.cpp

---
+++ array_int.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main() {
+
+	int numbers[5] = {22, 15, 2, 32,19};
+
+	std::cout << numbers[0] << "\n";
+	std::cout << numbers[1] << "\n";
+	std::cout << numbers[2] << "\n";
+	std::cout << numbers[3] << "\n";
+	std::cout << numbers[4] << std::endl;
+
+
+	return 0;
+}
