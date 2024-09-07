e54fc7a862a469d27c454049dfb98ea686cacc46
imFAHADcom <contact@imfahad.com>
2024-09-07 23:40:01 +0300

    ADD array_string.cpp

---
+++ array_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+
+	std::string name = "im FAHAD com";
+
+
+	std::cout << name[0] << "\n";
+	std::cout << name[3] << "\n";
+	std::cout << name[7] << std::endl;
+
+
+	return 0;
+}
