3940f03a4756f06c10b140c499bfbbe035c654f8
imFAHADcom <contact@imfahad.com>
2025-01-28 17:05:28 +0300

    ADD a_to_z_nested_for_loops.cpp

---
+++ a_to_z_nested_for_loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+	for(char i = 65; i <= 90; ++ i)
+	{
+		for(char letter = 65; letter <= 90; ++letter)
+		{
+			std::cout << i << letter  << std::endl;
+			
+		}
+	}
+
+
+	return 0;
+}
