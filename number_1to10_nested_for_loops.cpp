adcccb5be2f269b43407613e538e8d8a98e27429
imFAHADcom <contact@imfahad.com>
2025-01-28 17:03:57 +0300

    ADD number_1to10_nested_for_loops.cpp

---
+++ number_1to10_nested_for_loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+	for(int i = 1; i <= 10; ++i) 
+	{
+		for(int number = 1; number <= i; ++number)
+		{
+			std::cout << number;
+		}
+		std::cout << std::endl;
+	}
+
+
+	return 0;
+}
