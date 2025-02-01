0492461497c0636d0bda33b1da2558f2669fa658
imFAHADcom <contact@imfahad.com>
2025-02-01 22:33:23 +0300

    ADD print_numbers_from_n_to_1_do_while_loop.cpp

---
+++ print_numbers_from_n_to_1_do_while_loop.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+signed int getUserNumber()
+{
+	signed int number;
+
+	std::cout << "Enter number, Please?\n";
+	std::cin >> number;
+	std::cout << "***************************" << std::endl;
+
+	return number;
+}
+
+
+void printNumber(signed int number)
+{
+	signed int i = 1;
+
+	do
+	{
+
+		std::cout << number << std::endl;
+		number--;
+	} while(number >= i);
+
+}
+
+
+
+int main()
+{
+	printNumber(getUserNumber());
+
+
+
+	return 0;
+}
