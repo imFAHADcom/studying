284ec0889ebc9cebb4d74e7d4ad252ded8840ed8
imFAHADcom <contact@imfahad.com>
2025-02-04 16:08:30 +0300

    ADD printmyname_function.cpp

---
+++ printmyname_function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+void printName(std::string name)
+{
+
+	std::cout << "My name is: " << name << std::endl;
+
+}
+
+int main()
+{
+
+	printName("Fahad");
+
+
+
+	return 0;
+}
